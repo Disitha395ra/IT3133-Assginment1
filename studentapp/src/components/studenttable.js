29b3396c739810bf223fd71d98b1f960a4b78828
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2024-11-17 17:00:30 +0000

    student table changes

--- studentapp/src/components/studenttable.js
+++ studentapp/src/components/studenttable.js
@@ -1,8 +1,24 @@
+import '../assests/css/compo.css'
 export default function studenttable(props){
 
     return(
-        <div>
+        <div className="outerDiv">
+            <div className="leftDiv">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>FirstName</td>
+                            <td>LastName</td>
+                            <td>Course</td>
+                            <td>Country</td>
+                            <td>Profile</td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div className="rightDiv">
 
+            </div>
         </div>
     )
 }
